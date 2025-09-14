e202865b87a5bbdb11e7be1d31c23a32b89d8014
Денис Драйдт <denisdreidt@gmail.com>
2025-09-14 15:54:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Крестики-нолики</h1>
+      <p>Игра доступна через Telegram-бот</p>
+    </div>
+  );
+}
+
+export default App;
